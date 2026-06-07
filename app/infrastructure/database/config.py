e581262a2f6c6597c19770d420fde02428602e7d
Denis <ar.denis.by@gmail.com>
2026-06-07 16:45:20 +0300

    feat(database): add database config
    - add base class
    - add database config class
    - add requirements for database
    - configurate env file for database + add .env.example

---
+++ app/infrastructure/database/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DatabaseConfig(BaseSettings):
+    """Класс для сборки конфигурации бд"""
+
+    DB_USER: str
+    DB_PASS: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='UTF-8', extra='ignore')
+
+    @property
+    def database_url(self) -> str:
+        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
