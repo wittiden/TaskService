e4720b82a12fe9dc4c81dd0304b0862adc23c337
Denis <ar.denis.by@gmail.com>
2026-07-30 22:38:36 +0300

    hotfix

--- app/modules/auth/jwt_config.py
+++ app/modules/auth/jwt_config.py
@@ -7,14 +7,14 @@ class TokenConfig(BaseSettings):
     """Класс для конфигурации токенов"""
 
     ACCESS_TOKEN_ALGORITHM: str = 'RS256'
-    ACCESS_TOKEN_PRIVATE_KEY_PATH: str = 'certs/access-private'
-    ACCESS_TOKEN_PUBLIC_KEY_PATH: str = 'certs/access-public'
+    ACCESS_TOKEN_PRIVATE_KEY_PATH: str = 'certs/access-private.pem'
+    ACCESS_TOKEN_PUBLIC_KEY_PATH: str = 'certs/access-public.pem'
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 5
     ACCESS_TOKEN_AUDIENCE: str = 'access-api'
 
     REFRESH_TOKEN_ALGORITHM: str = 'RS256'
-    REFRESH_TOKEN_PRIVATE_KEY_PATH: str = 'certs/refresh-private'
-    REFRESH_TOKEN_PUBLIC_KEY_PATH: str = 'certs/refresh-public'
+    REFRESH_TOKEN_PRIVATE_KEY_PATH: str = 'certs/refresh-private.pem'
+    REFRESH_TOKEN_PUBLIC_KEY_PATH: str = 'certs/refresh-public.pem'
     REFRESH_TOKEN_EXPIRE_DAYS: int = 1
     REFRESH_TOKEN_AUDIENCE: str = 'refresh-api'
     REFRESH_TOKEN_VERSION: int = 1
