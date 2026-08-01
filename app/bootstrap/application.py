171194ee5cbf96a614bdbb4b02186d705a4675dd
Denis <ar.denis.by@gmail.com>
2026-08-01 21:26:06 +0300

    feat(metrics): rebuild prometheus file struct

--- app/bootstrap/application.py
+++ app/bootstrap/application.py
@@ -1,12 +1,12 @@
 from dishka import AsyncContainer
 from dishka.integrations.fastapi import setup_dishka
 from fastapi import FastAPI
-from prometheus_fastapi_instrumentator import Instrumentator
 
 from app.bootstrap.handlers import setup_handlers
 from app.bootstrap.middlewares import setup_middlewares
 from app.bootstrap.routers import setup_routers
 from app.common.limiter.config import limiter
+from app.common.observability.metrics import setup_prometheus
 from app.container.container import async_container
 from app.infrastructure.http.lifespan import lifespan
 from app.infrastructure.http.middleware.CORS.config import cors_config
@@ -14,13 +14,20 @@ from app.infrastructure.http.middleware.CORS.setup import setup_cors
 
 
 def setup_application(container: AsyncContainer = async_container) -> FastAPI:
-    app = FastAPI(title='TaskService', version='2.0.0', lifespan=lifespan)
+    app = FastAPI(
+        title='TaskService',
+        version='2.0.0',
+        summary='TaskService API',
+        description=(
+            'TaskService is a production-ready REST API for task management '
+            'with a full MELT observability stack, built on FastAPI with '
+            'asynchronous architecture, Dependency Injection, and a modular '
+            'structure.'
+        ),
+        lifespan=lifespan,
+    )
 
-    Instrumentator(
-        should_group_status_codes=True,
-        should_ignore_untemplated=True,
-        excluded_handlers=['/health', '/metrics'],
-    ).instrument(app).expose(app, include_in_schema=True, should_gzip=True)
+    setup_prometheus(app)
 
     app.state.limiter = limiter
 
---
+++ app/common/observability/metrics.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.routing import APIRoute
+from prometheus_fastapi_instrumentator import (
+    Instrumentator,
+    metrics,
+)
+
+
+def setup_prometheus(app: FastAPI) -> Instrumentator:
+    prometheus_instrument = Instrumentator(
+        should_group_status_codes=True,
+        should_ignore_untemplated=True,
+        excluded_handlers=['/health', '/metrics'],
+    )
+
+    prometheus_instrument.instrument(app).expose(
+        app, should_gzip=True, endpoint='/metrics', include_in_schema=True
+    )
+
+    prometheus_instrument.add(
+        metrics.latency(),
+        metrics.request_size(),
+        metrics.response_size(),
+        metrics.combined_size(),
+    )
+
+    for route in app.routes:
+        if isinstance(route, APIRoute) and route.path == '/metrics':
+            route.tags = ['observability']
+
+    return prometheus_instrument
